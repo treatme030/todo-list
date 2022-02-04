1f88e50ba49a2bc6361078c729991d7d337de852
treatme030 <treatme8192@gmail.com>
2022-02-04 19:24:56 +0900

    feat: Input 컴포넌트 테스트 코드 작성

---
+++ src/components/Input/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import 'jest-styled-components';
+import { Input } from './index';
+
+describe('<Input />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<Input value="default value" />);
+
+    const input = screen.getByDisplayValue('default value');
+    expect(input).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+  it('renders placeholder correctly', () => {
+    render(<Input placeholder="default placeholder" />);
+
+    const input = screen.getByPlaceholderText('default placeholder');
+    expect(input).toBeInTheDocument();
+  });
+  it('changes the data', () => {
+    render(<Input placeholder="default placeholder" />);
+
+    const input = screen.getByPlaceholderText('default placeholder') as HTMLInputElement;
+    userEvent.type(input, 'study react');
+    expect(input.value).toBe('study react');
+  })
+});
