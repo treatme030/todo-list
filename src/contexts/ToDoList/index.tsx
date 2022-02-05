34f233b41295f9eb3fa5a60ef8c25bd2cb5864d7
treatme030 <treatme8192@gmail.com>
2022-02-05 14:30:28 +0900

    feat: localStorage에 데이터 저장하고, 가져오기

--- src/contexts/ToDoList/index.tsx
+++ src/contexts/ToDoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 interface Context {
   readonly toDoList: string[];
@@ -21,18 +21,32 @@ const ToDoListProvider = ({ children }: Props): JSX.Element => {
 
   const addToDo = (toDo: string): void => {
     if(toDo){
-      setToDoList([
-        ...toDoList,
-        toDo
-      ]);
+      // setToDoList([
+      //   ...toDoList,
+      //   toDo
+      // ]);
+      const newList = [...toDoList, toDo];
+      localStorage.setItem('ToDoList', JSON.stringify(newList));
+      setToDoList(newList);
     }
   }
 
   const deleteToDo = (index: number): void => {
-    setToDoList(prev => [
-      ...prev.filter((item, idx) => idx !== index)
-    ]);
+    // setToDoList(prev => [
+    //   ...prev.filter((item, idx) => idx !== index)
+    // ]);
+    let list = [...toDoList];
+    list.splice(index, 1);
+    localStorage.setItem('ToDoList', JSON.stringify(list));
+    setToDoList(list);
   }
+
+  useEffect(() => {
+    const list = localStorage.getItem('ToDoList');
+    if(list){
+      setToDoList(JSON.parse(list));
+    }
+  },[])
 
   return (
     <ToDoListContext.Provider
