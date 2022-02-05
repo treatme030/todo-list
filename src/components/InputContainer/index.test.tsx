2fa563cf2d1f0d0f479f4e3615e17967f34c79c5
treatme030 <treatme8192@gmail.com>
2022-02-05 23:54:39 +0900

    feat: InputContainer 테스트 코드 작성

---
+++ src/components/InputContainer/index.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import "@testing-library/jest-dom/extend-expect";
+import 'jest-styled-components';
+import { InputContainer } from './index';
+import { ToDoListProvider } from 'contexts';
+
+describe('<InputContainer />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<InputContainer />);
+
+    const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+    expect(input).toBeInTheDocument();
+    const addButton = screen.getByText('추가');
+    expect(addButton).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+  it('empties data after adding data', () => {
+    render(<InputContainer />);
+
+    const input = screen.getByPlaceholderText('할 일을 입력해 주세요') as HTMLInputElement;
+    const addButton = screen.getByText('추가');
+    expect(input.value).toBe('');
+
+    userEvent.type(input, 'study react 1');
+    expect(input.value).toBe('study react 1');
+    userEvent.click(addButton);
+    expect(input.value).toBe('');
+  });
+  it('adds input data to localStorage via Context', () => {
+    render(
+      <ToDoListProvider>
+        <InputContainer />
+      </ToDoListProvider>
+    );
+
+    const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+    const addButton = screen.getByText('추가');
+    expect(localStorage.getItem('ToDoList')).toBeNull();
+
+    userEvent.type(input, 'study react 1');
+    userEvent.click(addButton);
+    expect(localStorage.getItem('ToDoList')).toBe('["study react 1"]');
+  })
+});
+
