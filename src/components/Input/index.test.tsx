c4f94f3d71fac25e9202521d8455dc77d73c7685
treatme030 <treatme8192@gmail.com>
2022-02-05 10:12:10 +0900

    add: jest-dom/extend-expect

--- src/components/Input/index.test.tsx
+++ src/components/Input/index.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
+import "@testing-library/jest-dom/extend-expect";
 import 'jest-styled-components';
 import { Input } from './index';
 
