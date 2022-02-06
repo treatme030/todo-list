09e99752c5eeac95d4de9330cb6712fac6796e5b
treatme030 <treatme8192@gmail.com>
2022-02-06 21:22:12 +0900

    feat: 상세페이지로 이동하는 라우터 테스트 코드 작성

--- src/components/ToDoItem/index.test.tsx
+++ src/components/ToDoItem/index.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import "@testing-library/jest-dom/extend-expect";
@@ -7,10 +8,15 @@ import { ToDoItem } from './index';
 
 describe('<ToDoItem />', () => {
   it('renders component correctly', () => {
-    const { container } = render(<ToDoItem label="default value" />);
+    const { container } = render(
+      <Router>
+        <ToDoItem id={1} label="default value" />
+      </Router>
+    );
 
     const toDoItem = screen.getByText('default value');
     expect(toDoItem).toBeInTheDocument();
+    expect(toDoItem.getAttribute('href')).toBe('/detail/1');
 
     const deleteButton = screen.getByText('삭제');
     expect(deleteButton).toBeInTheDocument();
@@ -19,11 +25,39 @@ describe('<ToDoItem />', () => {
   });
   it('clicks the delete button', () => {
     const handleClick = jest.fn();
-    render(<ToDoItem label="default value" onDelete={handleClick} />);
+    render(
+      <Router>
+        <ToDoItem id={1} label="default value" onDelete={handleClick} />
+      </Router>
+    );
 
     const deleteButton = screen.getByText('삭제');
     expect(handleClick).toHaveBeenCalledTimes(0);
     userEvent.click(deleteButton);
     expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+  it('clicks the link', () => {
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return (
+        <div>
+          <div>{pathname}</div>
+          <ToDoItem id={1} label="default value" />
+        </div>
+      );
+    };
+
+    render(
+      <Router>
+        <TestComponent />
+      </Router>
+    );
+
+    const pathName = screen.getByText('/');
+    expect(pathName).toBeInTheDocument();
+    const toDoItem = screen.getByText('default value');
+
+    userEvent.click(toDoItem);
+    expect(pathName.textContent).toBe('/detail/1');
   });
 });
