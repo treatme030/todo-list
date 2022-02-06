8f5d41f2d9c72e553c8ba366567702469fbf285a
treatme030 <treatme8192@gmail.com>
2022-02-06 23:59:38 +0900

    feat: Detail 페이지 테스트 코드 작성

---
+++ src/pages/Detail/index.test.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Router, Route, useLocation } from "react-router-dom";
+import { createMemoryHistory } from 'history';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import 'jest-styled-components';
+import { ToDoListProvider } from "contexts";
+import { Detail } from "./index";
+
+describe('<Detail />', () => {
+  it('renders component correctly', () => {
+    const history = createMemoryHistory();
+    history.push('/detail/0');
+    localStorage.setItem('ToDoList', '["ToDo 1"]');
+
+    const { container } = render(
+      <ToDoListProvider>
+        <Router history={history}>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Router>
+      </ToDoListProvider>
+    );
+
+    const toDoItem = screen.getByText('ToDo 1');
+    expect(toDoItem).toBeInTheDocument();
+    const deleteButton = screen.getByText('삭제');
+    expect(deleteButton).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+  it('deletes ToDo data', () => {
+    const history = createMemoryHistory();
+    history.push('/');
+    history.push('/detail/0');
+    localStorage.setItem('ToDoList', '["ToDo 1"]');
+
+    const TestComponent = () => {
+      const { pathname } = useLocation();
+      return (
+        <div>{pathname}</div>
+      );
+    };
+
+    render(
+      <ToDoListProvider>
+        <Router history={history}>
+          <TestComponent />
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Router>
+      </ToDoListProvider>
+    );
+
+    const url = screen.getByText('/detail/0');
+    expect(url).toBeInTheDocument();
+    const toDoItem = screen.getByText('ToDo 1');
+    expect(toDoItem).toBeInTheDocument();
+    const deleteButton = screen.getByText('삭제');
+    expect(deleteButton).toBeInTheDocument();
+
+    userEvent.click(deleteButton);
+    expect(url.textContent).toBe('/');
+    expect(toDoItem).not.toBeInTheDocument();
+    expect(deleteButton).not.toBeInTheDocument();
+  });
+});
