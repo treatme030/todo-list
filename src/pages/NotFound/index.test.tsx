e5f40a8815d43c1771dc09d471c2f3ac48717491
treatme030 <treatme8192@gmail.com>
2022-02-07 00:34:26 +0900

    feat: NotFound 페이지 테스트 코드 작성

---
+++ src/pages/NotFound/index.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import 'jest-styled-components';
+import { NotFound } from "./index";
+
+describe('<NotFound />', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<NotFound />);
+
+    const message = screen.getByText('Not Found');
+    expect(message).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+});
